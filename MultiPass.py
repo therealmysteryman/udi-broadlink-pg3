dff4554979d1e765e0d8f86cb64f9df13ba031ca
automationgeek <jf.tremblay@gmail.com>
2021-11-01 10:41:03 -0400

    Update MultiPass.py
    Added SwitchBot Curtain

--- MultiPass.py
+++ MultiPass.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 
 """
-This is a NodeServer for Polyglot v2 written in Python3
-It will contain many different custom objects that will connect random technology.
------------------------------------------------------------------------------------
 Import the polyglot interface module.
 Also using the broadlink module from: https://github.com/mjg59/python-broadlink
         The MIT License (MIT)
@@ -12,54 +9,18 @@ Also using the broadlink module from: https://github.com/mjg59/python-broadlink
 """
 import udi_interface
 import broadlink
-
+from switchbot import SwitchBot
 import sys
 import json
 # Import the Dictionary that contains all nodes and RFCodes for each. (RFCodes.py)
 from RFCodes import RFCodes
-#import logging
-#import urllib3
 
-"""
-polyinterface has a LOGGER that is created by default and logs to:
-logs/debug.log
-You can use LOGGER.info, LOGGER.warning, LOGGER.debug, LOGGER.error levels as needed.
-"""
 LOGGER = udi_interface.LOGGER
 SERVERDATA = json.load(open('server.json'))
 VERSION = SERVERDATA['credits'][0]['version']
-# IF you want a different log format than the current default
-#polyinterface.LOG_HANDLER.set_log_format('%(asctime)s %(threadName)-10s %(name)-18s %(levelname)-8s %(module)s:%(funcName)s: %(message)s')
 
-""" Define My MultiPass! Controller Node Class"""
 class Controller(udi_interface.Node):
-    """
-    The Controller Class is the primary node from an ISY perspective. It is a Superclass
-    of polyinterface.Node so all methods from polyinterface.Node are available to this
-    class as well.
 
-    Class Variables:
-    self.nodes: Dictionary of nodes. Includes the Controller node. Keys are the node addresses
-    self.name: String name of the node
-    self.address: String Address of Node, must be less than 14 characters (ISY limitation)
-    self.polyConfig: Full JSON config dictionary received from Polyglot for the controller Node
-    self.added: Boolean Confirmed added to ISY as primary node
-    self.config: Dictionary, this node's Config
-
-    Class Methods (not including the Node methods):
-    start(): Once the NodeServer config is received from Polyglot this method is automatically called.
-    addNode(polyinterface.Node, update = False): Adds Node to self.nodes and polyglot/ISY. This is called
-        for you on the controller itself. Update = True overwrites the existing Node data.
-    updateNode(polyinterface.Node): Overwrites the existing node data here and on Polyglot.
-    delNode(address): Deletes a Node from the self.nodes/polyglot and ISY. Address is the Node's Address
-    longPoll(): Runs every longPoll seconds (set initially in the server.json or default 10 seconds)
-    shortPoll(): Runs every shortPoll seconds (set initially in the server.json or default 30 seconds)
-    query(): Queries and reports ALL drivers for ALL nodes to the ISY.
-    getDriver('ST'): gets the current value from Polyglot for driver 'ST' returns a STRING, cast as needed
-    runForever(): Easy way to run forever without maxing your CPU or doing some silly 'time.sleep' nonsense
-                  this joins the underlying queue query thread and just waits for it to terminate
-                  which never happens.
-    """
     mybroadlink = None
 
     def __init__(self, polyglot, primary, address, name):
@@ -76,8 +37,18 @@ class Controller(udi_interface.Node):
     
     def parameterHandler(self, params):
         self.poly.Notices.clear()
-        self.discover()
-    
+                
+        try:
+            if 'token' in params:
+                self.token = params['token']
+            else:
+                LOGGER.error('SwitchBot requires \'token\' parameters to be specified in custom configuration.')
+                return False
+               
+            self.discover()
+        except Exception as ex:
+            LOGGER.error('Error starting MultiPass NodeServer: %s', str(ex))
+
     def start(self):
         LOGGER.info('Starting MultiPass NodeServer version {}'.format(VERSION))
         self.setDriver('ST', 1)
@@ -86,7 +57,15 @@ class Controller(udi_interface.Node):
         pass
     
     def discover(self, *args, **kwargs):
+        # broadlink
         self.connectbl()
+        # switchbot curtain
+        switchbot = SwitchBot(token=self.token)
+        sbCurtainId = [ '5F0B798AEF91','5F0B798AEF91','5F0B798AEF91' ]
+        
+        for node in sbCurtainId :
+            if not self.poly.getNode(node):
+                self.poly.addNode(sbCurtain(self.poly, self.address, node, node, switchbot.device(id=node)))        
 
     def connectbl(self, command=None):
         
@@ -161,36 +140,9 @@ class Controller(udi_interface.Node):
         {'driver': 'GV1', 'value': 0, 'uom': 56} ] 
 
 class omniamotor(udi_interface.Node):
-    """
-    This is the class that all the Nodes will be represented by. You will add this to
-    Polyglot/ISY with the controller.addNode method.
-
-    Class Variables:
-    self.primary: String address of the Controller node.
-    self.parent: Easy access to the Controller Class from the node itself.
-    self.address: String address of this Node 14 character limit. (ISY limitation)
-    self.added: Boolean Confirmed added to ISY
-
-    Class Methods:
-    start(): This method is called once polyglot confirms the node is added to ISY.
-    setDriver('ST', 1, report = True, force = False):
-        This sets the driver 'ST' to 1. If report is False we do not report it to
-        Polyglot/ISY. If force is True, we send a report even if the value hasn't changed.
-    reportDrivers(): Forces a full update of all drivers to Polyglot/ISY.
-    query(): Called when ISY sends a query request to Polyglot for this specific node
-    """
+ 
     def __init__(self, controller, primary, address, name, dev):
-        """
-        Optional.
-        Super runs all the parent class necessities. You do NOT have
-        to override the __init__ method, but if you do, you MUST call super.
-
-        :param controller: Reference to the Controller class
-        :param primary: Controller address
-        :param address: This nodes address
-        :param name: This nodes name
-        :param rfc: The Up / Down / Stop byte codes for RF Packets.
-        """
+    
         super(omniamotor, self).__init__(controller, primary, address, name)
         self.ctrl = controller
         self.pri = primary
@@ -200,37 +152,7 @@ class omniamotor(udi_interface.Node):
         
         controller.subscribe(controller.START, self.start, address)
 
-    def shortPoll(self):
-        LOGGER.debug('shortPoll')
-        """
-        if int(self.getDriver('ST')) == 1:
-            self.setDriver('ST',0)
-        else:
-            self.setDriver('ST',1)
-        """
-        LOGGER.debug('Omnia %s: ST=%s',self.name,self.getDriver('ST'))
-
-    def longPoll(self):
-        LOGGER.debug('longPoll')
-
     def start(self):
-        """
-        Optional.
-        This method is run once the Node is successfully added to the ISY
-        and we get a return result from Polyglot. Only happens once.
-        """
-        '''
-        LOGGER.debug('%s: get ST=%s', self.lpfx, self.getDriver('ST'))
-        self.setDriver('ST', 1)
-        LOGGER.debug('%s: get ST=%s', self.lpfx, self.getDriver('ST'))
-        self.setDriver('ST', 0)
-        LOGGER.debug('%s: get ST=%s', self.lpfx, self.getDriver('ST'))
-        self.setDriver('ST', 1)
-        LOGGER.debug('%s: get ST=%s', self.lpfx, self.getDriver('ST'))
-        self.setDriver('ST', 0)
-        LOGGER.debug('%s: get ST=%s', self.lpfx, self.getDriver('ST'))
-        self.http = urllib3.PoolManager()
-        '''
         self.setDriver('ST', 1)
 
     def cmd_up(self,command):
@@ -248,41 +170,43 @@ class omniamotor(udi_interface.Node):
         #LOGGER.info('RFCode Lookup for {}:{}.'.format(self.name,RFCodes[self.name][0]))
         self.dev.send_data(RFCodes[self.name][0])
 
-    '''
-    def query(self,command=None):
-        """
-        Called by ISY to report all drivers for this node. This is done in
-        the parent class, so you don't need to override this method unless
-        there is a need.
-        """
-        self.reportDrivers()
-    '''
-
     #Hints See: https://github.com/UniversalDevicesInc/hints
     #hint = [1,2,3,4]
     drivers = [{'driver': 'ST', 'value': 0, 'uom': 2}]
-    """
-    Optional.
-    This is an array of dictionary items containing the variable names(drivers)
-    values and uoms(units of measure) from ISY. This is how ISY knows what kind
-    of variable to display. Check the UOM's in the WSDK for a complete list.
-    UOM 2 is boolean so the ISY will display 'True/False'
-    """
-    id = 'omniamotor'
-    """
-    id of the node from the nodedefs.xml that is in the profile.zip. This tells
-    the ISY what fields and commands this node has.
-    """
+ 
     commands = {
                     'BUP': cmd_up,
                     'BDOWN': cmd_down,
                     'BSTOP': cmd_stop
                 }
-    """
-    This is a dictionary of commands. If ISY sends a command to the NodeServer,
-    this tells it which method to call. DON calls setOn, etc.
-    """
 
+class sbCurtain(udi_interface.Node):
+ 
+    def __init__(self, controller, primary, address, name, dev):
+    
+        super(sbCurtain, self).__init__(controller, primary, address, name)
+        self.dev = dev
+        
+        controller.subscribe(controller.START, self.start, address)
+
+    def start(self):
+        self.setDriver('ST', 1)
+
+    def cmd_open(self,command):
+        self.dev.command('open')
+
+    def cmd_close(self,command):
+        self.dev.command('close')
+
+        
+    #Hints See: https://github.com/UniversalDevicesInc/hints
+    #hint = [1,2,3,4]
+    drivers = [{'driver': 'ST', 'value': 0, 'uom': 2}]
+ 
+    commands = {
+                    'BOPEN': cmd_open,
+                    'BCLOSE': cmd_close
+                }
 
 if __name__ == "__main__":
     try:
