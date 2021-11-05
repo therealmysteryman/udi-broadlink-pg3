1287dfac8d43488a5afda154caeaed5b8068540d
automationgeek <jf.tremblay@gmail.com>
2021-11-04 22:57:31 -0400

    Update MultiPass.py

--- MultiPass.py
+++ MultiPass.py
@@ -65,7 +65,7 @@ class Controller(udi_interface.Node):
         
         for node in sbCurtainId :
             if not self.poly.getNode(node):
-                self.poly.addNode(sbCurtain(self.poly, self.address, node, node+"_", switchbot.device(id=node)))        
+                self.poly.addNode(sbCurtain(self.poly, self.address, node+"_", node , switchbot.device(id=node)))        
 
     def connectbl(self, command=None):
         
